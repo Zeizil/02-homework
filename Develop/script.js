ba073af41957b08bfcba7aae72f39077ce469678
Zeizil <ekphilbrook@gmail.com>
2022-10-24 21:24:56 -0400

    Finshed logic for password generator, need to set up checklist

--- Develop/script.js
+++ Develop/script.js
@@ -21,13 +21,41 @@ function generatePassword(pwlength, reqLetters, reqNums, reqSpec){
       // yes letters, numbers
       if(reqSpec){
         // yes special characters - all three
-
+        for (let a = 0; a < pwlength; a++){
+          // coin flip to decide letter, number, or special character
+          let decider = Math.floor(Math.random() * 3);
+          // 0 is letter
+          if(decider == 0){
+            let letter = Math.floor(Math.random() * alphabet.length + 1);
+            pw += alphabet.charAt(letter);
+          }
+          // 1 is number
+          else if(decider == 1){
+            let num = Math.floor(Math.random() * 10);
+            pw += toString(num);
+          }
+          // 2 is special character
+          else if(decider == 2){
+            let symbol = Math.floor(Math.random() * spec.length + 1);
+      pw += spec.charAt(symbol);
+          }
+        }
       }
       else{
         // only letters and numbers
         for (let b = 0; b < pwlength; b++){
+          // coin flip to decide letter or number
           let decider = Math.floor(Math.random() * 2);
-          
+          // 0 is letter
+          if(decider == 0){
+            let letter = Math.floor(Math.random() * alphabet.length + 1);
+            pw += alphabet.charAt(letter);
+          }
+          // 1 is number
+          else if(decider == 1){
+            let num = Math.floor(Math.random() * 10);
+            pw += toString(num);
+          }
         }
       }
     }
@@ -43,7 +71,20 @@ function generatePassword(pwlength, reqLetters, reqNums, reqSpec){
     // no letters, yes numbers
     if(reqSpec){
       // no letters, yes other two
-
+      for (let d = 0; d < pwlength; d++){
+        // coin flip to decide number or special
+        let decider = Math.floor(Math.random() * 2);
+        // 0 is number
+        if(decider == 0){
+          let num = Math.floor(Math.random() * 10);
+          pw += toString(num);
+        }
+        // 1 is special
+        else if(decider == 1){
+          let symbol = Math.floor(Math.random() * spec.length + 1);
+          pw += spec.charAt(symbol);
+        }
+      }
     }
     else{
       // only numbers
